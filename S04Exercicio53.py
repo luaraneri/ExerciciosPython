68e52b23fb268edd3b34f97f8556fe23184d8ae8
luaraneri <69724280+luaraneri@users.noreply.github.com>
2021-08-02 12:02:04 -0300

    Create S04Exercicio53.py

---
+++ S04Exercicio53.py
@@ -0,0 +1,7 @@
+c = int(input("Insira o comprimento do terreno (metro): "))
+l = int(input("Insira a largura do terreno (metro): "))
+v = int(input("Insira o preço do metro de tela: "))
+
+preco = ((2*c)+(2*l))*v
+
+print(preco)
