f3cbaf9ce1ff2aa7e3dd37b8be49d61cb2f3b34d
luaraneri <69724280+luaraneri@users.noreply.github.com>
2021-08-02 12:01:06 -0300

    Create S04Exercicio32.py

---
+++ S04Exercicio32.py
@@ -0,0 +1,6 @@
+valor = int(input('Insira um número inteiro: '))
+
+num_um = (valor * 3) + 1
+num_dois = (valor * 2) - 1
+
+print(f'Soma do sucessor do triplo do número digitado com o antecessor de seu dobro: {num_um+num_dois}')
